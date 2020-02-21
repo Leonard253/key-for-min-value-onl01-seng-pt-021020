4faa13b041d8ffe3f138e8dfc61e205bcb1f6c9d
Trey Williams <leonardwilliamsiii20@gmail.com>
2020-02-21 06:55:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,3 +6,10 @@ lowest_value = 0
   lowest_key = nil
   
   name_hash.each do |key,value|
+  if lowest_value == 0 || value < lowest_value
+     lowest_value = value 
+      lowest_key = key
+    end
+  end
+  lowest_key
+end
